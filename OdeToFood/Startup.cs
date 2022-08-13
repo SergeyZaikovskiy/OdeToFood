9703f5d11e910c0bc1028708046b657e5e41bf50
SergeyZaikovskiy <SergeyZaikovskiy>
2022-08-13 12:20:58 +0500

    Add servicies

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OdeToFood.Data;
+using OdeToFood.Data.Implementations;
+using OdeToFood.Data.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +27,14 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<OdeToFoodDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
+            });
+
+            // services.AddScoped<IRestaurantData, SqlRestaurantData>();
+            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+
             services.AddRazorPages();
         }
 
