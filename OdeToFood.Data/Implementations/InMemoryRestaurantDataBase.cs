75acedb282701e6b9ddca0b01b148ea6ffb7718a
SergeyZaikovskiy <SergeyZaikovskiy>
2022-08-13 11:38:26 +0500

    Add project files.

---
+++ OdeToFood.Data/Implementations/InMemoryRestaurantDataBase.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Implementations
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        // Restaurant restaurant;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+                {
+                    new Restaurant{Id = 1, Name = "Dodo", Location = "Moscow", Cuisine = CuisineType.Italian},
+                    new Restaurant{Id = 2, Name = "Pelmeni", Location = "Chekyabinsk", Cuisine = CuisineType.Russian},
+                    new Restaurant{Id = 3, Name = "Starbucks", Location = "Washigton", Cuisine = CuisineType.American},
+                    new Restaurant{Id = 4, Name = "Chillis", Location = "Mexico", Cuisine = CuisineType.Mexican},
+                };
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(restaurant => restaurant.Name);
+        }
+
+        public Restaurant GetRestaurantById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantByName(string name)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant AddRestaurant(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+
+            return newRestaurant;
+        }
+
+        public Restaurant UpdateRestaurantInfo(Restaurant restaurant)
+        {
+            var rest = restaurants.SingleOrDefault(r => r.Id == restaurant.Id);
+
+            if (restaurant != null)
+            {
+                rest.Name = restaurant.Name;
+                rest.Location = restaurant.Name;
+                rest.Cuisine = restaurant.Cuisine;
+            }
+
+            return rest;
+        }
+    }
+}
---
+++ OdeToFood.Data/Interfaces/IRestaurantData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Interfaces
+{
+
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+
+        IEnumerable<Restaurant> GetRestaurantByName(string name);
+
+        Restaurant GetRestaurantById(int id);
+
+        Restaurant UpdateRestaurantInfo(Restaurant restaurant);
+
+        Restaurant AddRestaurant(Restaurant newRestaurant);
+
+        int Commit();
+    }
+
+}
