5f41d61ab7364e4858490811eb6b3fae25149fda
SergeyZaikovskiy <SergeyZaikovskiy>
2022-08-14 10:39:32 +0500

    Create first migration

--- OdeToFood.Data/OdeToFoodDbContext.cs
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -13,26 +13,6 @@ namespace OdeToFood.Data
 
         }
 
-        //public OdeToFoodDbContext(DbContextOptions options) : base(options)
-        //{
-        //}
-
-        //public OdeToFoodDbContext()
-        //{
-
-        //}
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder options)
-        //{
-        //    if (options.IsConfigured)
-        //    {
-        //        return;
-        //    }
-
-        //    options.UseSqlServer(_connectionString);
-        //}
-
-
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
