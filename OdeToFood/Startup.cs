03cf5fef5f8abf2774e267a23bcec5667b0afafc
SergeyZaikovskiy <SergeyZaikovskiy>
2022-08-21 20:47:57 +0500

    Add js tables to project

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -47,7 +47,7 @@ namespace OdeToFood
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -62,6 +62,8 @@ namespace OdeToFood
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseNodeModules(env);
+            app.UseCookiePolicy();
 
             app.UseRouting();
 
