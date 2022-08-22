cd0296bdd040f023fa3708eabae9382f06bfaa06
SergeyZaikovskiy <SergeyZaikovskiy>
2022-08-22 21:04:15 +0500

    Tries to fix startup

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -43,7 +43,7 @@ namespace OdeToFood
             });
 
             services.AddRazorPages();
-            services.AddControllers();
+            services.AddControllers();        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
